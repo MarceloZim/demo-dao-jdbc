070a86f513ad9bcdd9d49bcaf0277aca182b470f
MarceloZim <marcelod232015@gmail.com>
2023-07-31 21:48:41 -0300

    Sallary entitu class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,16 @@
 package application;
+import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Saller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Saller saller = new Saller(28, "Marcelo", "marcelod112233@gmail.com", new Date(), 3000.00, obj);
+		System.out.println(saller);
 
 	}
 
